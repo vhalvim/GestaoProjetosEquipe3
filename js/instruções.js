8900a2570f3a62c3aaad2aa5e25ac481636e2b4c
Lucas Torres <lukastorresu@gmail.com>
2026-05-21 09:35:36 -0300

    novas instruções para back/front-end

--- js/instruções.js
+++ js/instruções.js
@@ -1,11 +1,14 @@
-/*
-Para o front -
-Fazer um .map() no array data e criar os cards passando
-pokemon.name, pokemon.image_url e pokemon.types
 
-Para o back -
-Para pegar a imagem de cada Pokémon, montar a URL
-da imagem dinamicamente usando o id (como mostrei no JSON),
-já que a estrutura da URL do repositório oficial de imagens
-do Pokémon é previsível.
+/*
+INSTRUÇÕES PARA A EQUIPE:
+
+-> Backend:
+Os campos "height" e "weight" agora devem ser enviados já convertidos (divididos por 10) para metros e kg.
+O "stats" agora é um array de objetos contendo "name" e "value".
+Precisa consultar os endpoints /pokemon-species, /type e /evolution-chain da PokeAPI para preencher "description", "gender", "weaknesses" e "evolutions".
+
+-> Frontend:
+A propriedade da imagem mudou de "image_url" para apenas "image".
+A "description" pode vir com caracteres de quebra de linha (\n). Você precisará tratar isso no CSS/HTML (ex: white-space: pre-wrap) para o texto não ficar todo em uma linha só.
+O array de "evolutions" já traz o ID, Nome e a URL da Imagem prontos para você renderizar os cards menores na tela de detalhes.
 */
